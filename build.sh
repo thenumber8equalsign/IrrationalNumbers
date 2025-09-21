2786f4cca530ebb61a90155486a242782238af71
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 17:16:09 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 if command -v cmake &> /dev/null && command -v make &> /dev/null && command -v g++ &> /dev/null; then
     echo "cmake, make, and g++ are installed"
 else
-fi
     echo "You need to install cmake, make and g++ on your system"
+fi
 
 #cmake -DCMAKE_BUILD_TYPE=Debug ../src
 #make
