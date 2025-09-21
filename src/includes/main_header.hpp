02240391c3bf86fa7d3c507efa3b99b3b7585982
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 11:39:47 -0400

    Add boilerplate to main_header.hpp

---
+++ src/includes/main_header.hpp
@@ -0,0 +1,13 @@
+#ifndef MAIN_HEADER_HPP
+#define MAIN_HEADER_HPP
+
+#include <boost/multiprecision/mpfr.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
+
+namespace IrrationalNumbers {
+
+}
+
+
+#endif
