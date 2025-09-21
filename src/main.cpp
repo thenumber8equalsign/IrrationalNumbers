bdecd5a3139d97ab6fa6a5b4a7e288abe3b0fa7a
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 15:01:13 -0400

    Add testing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include "../includes/main_header.hpp"
+#include "includes/main_header.hpp"
 #include <iostream>
 
-int main() {
 
+int main() {
+    // Variables
+    IrrationalNumbers::BigFloat base;
+    IrrationalNumbers::BigInt exponent;
+    IrrationalNumbers::BigInt iBase;
+
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Give me a base: " << std::flush;
+    std::cin >> base;
+    std::cout << "Give me an exponent: " << std::flush;
+    std::cin >> exponent;
+
+    std::cout << "Power: " << IrrationalNumbers::bigFloatPower(base, exponent) << std::endl;
+
+    std::cout << "Give me a base: " << std::flush;
+    std::cin >> iBase;
+    std::cout << "Give me an exponent: " << std::flush;
+    std::cin >> exponent;
+
+    std::cout << "Power: " << IrrationalNumbers::bigIntPower(iBase, exponent) << std::endl;
+
+    return 0;
 }
