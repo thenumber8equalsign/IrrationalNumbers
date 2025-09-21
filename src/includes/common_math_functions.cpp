784582cd3fab849433a6b9eb00bc1cc3b51032d7
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 15:28:58 -0400

    Add bigFloatSqrt

--- src/includes/common_math_functions.cpp
+++ src/includes/common_math_functions.cpp
@@ -1,8 +1,47 @@
 #include "main_header.hpp"
+#include <iostream>
 #include <stdexcept>
+#include <cmath>
 
 namespace IrrationalNumbers {
-    BigFloat bigFloatSqrt(const BigFloat NUMBER, const uint64_t DIGITS) {
-        throw NotImplemented();
+    /**
+     * @brief given a positive integer calculate its square root to DIGITS precision
+     * @param NUMBER the number to compute the square root
+     * @param DIGITS number of decimal digits to return
+     * @param initialGuess the initial guess for newton's method
+     * @return the positive square root of NUMBER
+     * @throw std::out_of_range if NUMBER is negative
+     * @throw std::invalid_argument if DIGITS is negative
+     * @throw std::invalid_argument if guess is negative
+     */
+    BigFloat bigFloatSqrt(const BigInt NUMBER, const uint64_t DIGITS, BigInt initialGuess) {
+        // Check for valid arguments
+        if (NUMBER < 0) {
+            throw std::out_of_range("No support for complex numbers");
+        } else if (DIGITS < 0) {
+            throw std::invalid_argument("Negative DIGITS");
+        } else if (initialGuess < 0) {
+            throw std::invalid_argument("Negative guess");
+        }
+
+        // Variables
+        const BigFloat TWO = BigFloat(2);
+        const BigFloat F_NUMBER = BigFloat(NUMBER);
+        const BigFloat TEN_TO_DIGITS = bigFloatPower(10, DIGITS);
+
+        BigFloat guess = BigFloat(initialGuess);
+
+
+        for (uint64_t i = 0; i <= DIGITS; ++i) {
+            guess += F_NUMBER / guess;
+            guess /= TWO;
+
+            // * 100 / 100 for two decimal points
+            std::cout << "\x1b[2K\r" << std::floor(i / (double)DIGITS * 100 * 100) / 100 << "%" << std::flush;
+        }
+        std::cout << "\x1b[2K\r100%" << std::endl;
+        guess *= TEN_TO_DIGITS;
+        guess = boost::multiprecision::floor(guess);
+        return guess;
     }
 }
