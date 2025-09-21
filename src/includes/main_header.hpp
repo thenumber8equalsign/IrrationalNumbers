0978ac4e0b24cc76546b5ae4f150b25c7fc29a8f
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 12:41:01 -0400

    Add declarations

--- src/includes/main_header.hpp
+++ src/includes/main_header.hpp
@@ -1,12 +1,28 @@
 #ifndef MAIN_HEADER_HPP
 #define MAIN_HEADER_HPP
 
+#include <cstdint>
+
 #include <boost/multiprecision/mpfr.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
 
 namespace IrrationalNumbers {
+    // Type aliases
+    using BigFloat = boost::multiprecision::mpfr_float;
+    using BigInt = boost::multiprecision::cpp_int;
 
+    // Commonly used functions
+    BigFloat bigFloatPower(const BigFloat BASE, const BigInt EXPONENT);
+    BigFloat bigFloatSqrt(const BigFloat NUMBER, const uint64_t DIGITS, BigInt initialGuess = 0);
+
+    // Specific functions
+    BigFloat pi(const uint64_t DIGITS);
+    BigFloat euler(const uint64_t DIGITS);
+    BigFloat ln2(const uint64_t DIGITS, const bool COMPUTE_TERMS);
+
+    // Extra functions (not used by the program, but may be in the future)
+    BigFloat bigFloatLn(const uint64_t X, const uint64_t DIGITS);
 }
 
 
