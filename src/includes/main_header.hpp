a080232a07321ebadfc6c4161b6eba196b5f4def
thenumber8equalsign <ballari.son@gmail.com>
2025-09-21 15:00:57 -0400

    Moved definitions for inlined functions to the header file because the program did not compile

--- src/includes/main_header.hpp
+++ src/includes/main_header.hpp
@@ -32,5 +32,65 @@ namespace IrrationalNumbers {
 
     // Extra functions (not used by the program, but may be in the future)
     BigFloat bigFloatLn(const uint64_t X, const uint64_t DIGITS);
+
+
+
+    // Inline function definitions
+    /**
+     * @brief Calculates base^exponent
+     *
+     * This function takes a float as the base, and a positive integer as the exponent
+     *
+     * @param base the base of the power
+     * @param exponent the exponent of the power (non-negative)
+     * @return the value of base^exponent
+     * @throw std::invalid_argument if exponent is negative
+     */
+    inline BigFloat bigFloatPower(BigFloat base, BigInt exponent) {
+        // Check for valid arguments
+        if (exponent < 0) throw std::invalid_argument("Negative exponent");
+
+        // Variables
+        BigFloat result = 1;
+
+        while (exponent > 0) {
+            // If exponent is odd
+            if (exponent & 1) {
+                result *= base;
+            }
+
+            base *= base; // Square the base
+            exponent >>= 1; // Faster way of dividing by 2
+        }
+        return result;
+    }
+    /**
+     * @brief Calculates base^exponent
+     *
+     * This function takes an integer as the base, and a positive integer as the exponent
+     *
+     * @param base the base of the power
+     * @param exponent the exponent of the power (non-negative)
+     * @return the value of base^exponent
+     * @throw std::invalid_argument if exponent is negative
+     */
+    inline BigInt bigIntPower(BigInt base, BigInt exponent) {
+        // Check for valid arguments
+        if (exponent < 0) throw std::invalid_argument("Negative exponent");
+
+        // Variables
+        BigInt result = 1;
+
+        while (exponent > 0) {
+            // If exponent is odd
+            if (exponent & 1) {
+                result *= base;
+            }
+
+            base *= base;
+            exponent >>= 1; // Faster way of dividing by 2
+        }
+        return result;
+    }
 }
 #endif
