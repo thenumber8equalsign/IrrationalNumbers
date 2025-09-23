57c301910c486e769c615bfde943e8b04a7b4c85
thenumber8equalsign <ballari.son@gmail.com>
2025-09-23 12:42:09 -0400

    Update euler.cpp

--- src/includes/euler.cpp
+++ src/includes/euler.cpp
@@ -4,6 +4,19 @@
 
 namespace IrrationalNumbers {
     BigFloat euler(const uint64_t DIGITS) {
-        throw NotImplemented();
+        const BigInt TERMS = DIGITS * 2 + 2;
+        const BigFloat ONE = 1;
+
+        BigInt factorial = 1; // k!
+        BigFloat result = 1;
+
+        for (BigInt k = 1; k <= TERMS; ++k) {
+            factorial = factorial * k;
+            result = result + ONE / BigFloat(factorial);
+
+            std::cout << "\x1b[2K\r" << floor((double)k / (double)TERMS * 100 * 100) / 100 << std::flush;
+        }
+        std::cout << "\x1b[2K\r100.00%" << std::endl;
+        return result;
     }
 }
